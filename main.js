478cb19517d18813da37818a32a616c1ea05b85c
adiyogiinteractive <adiyogiinteractive@gmail.com>
2026-03-02 17:29:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+//Get Model Viewer
+const modelViewer = document.querySelector("model-viewer");
+console.log("modelViewer : " + modelViewer);
+
+modelViewer.addEventListener("load", async () => {
+  //Selecting the particular material, In this case material name is "Material #45"
+  //const material = modelViewer.model.getMaterialByName('Material #45');
+  
+  //Selecting the particular material, In this case material name is "PosterMaterial"
+  const material = modelViewer.model.getMaterialByName('Material #45');
+  
+  // Create a new texture from an image
+  //const texture = await modelViewer.createTexture('textures/woman.png');
+  
+  // Apply the new texture
+  //material.pbrMetallicRoughness.baseColorTexture.setTexture(texture);
+
+  //Before the material color was parrot green so we have to change it to white
+  material.pbrMetallicRoughness.setBaseColorFactor([1.0, 1.0, 1.0, 1.0]);
+});
+
+// Get a reference to the file input element
+const fileInput = document.getElementById('textureSelector');
+
+// Add a change event listener
+fileInput.addEventListener('change', async (event) => {
+  // The selected files are in the event.target.files FileList object
+  const files = event.target.files;
+  
+  // To get the first (and often only) selected file
+  const firstFile = files[0];
+
+  console.log(firstFile); 
+  // You can access file properties like name, size, and type
+  console.log('File Name:', firstFile.name);
+  console.log('File Size:', firstFile.size, 'bytes');
+  console.log('File Type:', firstFile.type);
+
+  
+  // Create a new texture from an image
+  const texture = await modelViewer.createTexture(URL.createObjectURL(firstFile));
+  
+  //Selecting the particular material, In this case material name is "Material #45"
+  const material = modelViewer.model.getMaterialByName('PosterMaterial');
+  // Apply the new texture
+  material.pbrMetallicRoughness.baseColorTexture.setTexture(texture);
+  //material.pbrMetallicRoughness.baseColorTexture.texture.source.setURI(texture);
+  
+});
